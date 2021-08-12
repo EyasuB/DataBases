896d4b12031d86f612b53b6bd40ae1c4167f9ef3
Eyasu Gebrehiwot <jgebrehiwot@gmail.com>
2021-08-12 14:37:27 -0500

    About page done

---
+++ workspace/workspace-vscode/ourcityLibrary/javascript/addBook.js
@@ -0,0 +1,49 @@
+window.onload = function () {
+    const addForm = document.getElementById("addForm");
+    addForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+      addBook(e);
+    });
+  };
+  
+  async function addBook(event) {
+    const response = await fetch(
+      "https://elibraryrestapi.herokuapp.com/elibrary/api/book/add",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          isbn: document.getElementById("inputISBN").value,
+          title: document.getElementById("inputTitle").value,
+          overdueFee: document.getElementById("inputOverdue").value,
+          publisher: document.getElementById("inputPublisher").value,
+          datePublished: document.getElementById("inputPubDate").value,
+        }),
+      }
+    );
+    document.getElementById("inputISBN").value = "";
+    document.getElementById("inputTitle").value = "";
+    document.getElementById("inputOverdue").value = "";
+    document.getElementById("inputPublisher").value = "";
+    document.getElementById("inputPubDate").value = "";
+    alert("Book was added");
+  }
+  
+  async function deleteBook(id) {
+    let result = confirm("This book will be removed. Do you want to continue?");
+    if (result) {
+      const response = await fetch(
+        `https://elibraryrestapi.herokuapp.com/elibrary/api/book/delete/${id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      alert("Book was deleted!");
+      location.reload();
+    }
+  }
---
+++ workspace/workspace-vscode/ourcityLibrary/javascript/library.js
@@ -0,0 +1,18 @@
+"use strict";
+const books = document.getElementById("books");
+
+(async function start() {
+  const res = await fetch(
+    "https://elibraryrestapi.herokuapp.com/elibrary/api/book/list"
+  );
+  const results = await res.json();
+  console.log(results);
+
+  results.forEach((item) => {
+    books.innerHTML += render(item);
+  });
+})();
+
+function render(item) {
+  return `<tr><td><strong id="counter"></td><td>${item.isbn}</td><td>${item.title}</td><td>${item.overdueFee}</td><td>${item.publisher}</td><td>${item.datePublished}</td><td><a href="editbook.html?bookId=${item.bookId}" class="nounderline" id="${item.bookId}">Edit</a></td><td><a class="nounderline" id="${item.bookId}" href="javascript:;" onclick="deleteBook(this.id)">Delete</a></td></tr>`;
+}
